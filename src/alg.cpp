476838efbc5b47813e9454508ba03443a6febfda
ShuvalovaEA <eshalos1207@gmail.com>
2024-03-03 09:28:19 +0300

    ADS2 add

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,49 @@
 // Copyright 2022 NNTU-CS
-#include <cstdint>
 #include "alg.h"
 
 
 double pown(double value, uint16_t n) {
-
+    double  result = 1; int i = 1;
+    while (i <= n) {
+        result *= value;
+        i += 1;
+    }
+    return result;
 }
 
 uint64_t fact(uint16_t n) {
-
+    int result = 1, i = 1;
+    while (i <= n) {
+        result *= i;
+        i += 1;
+    }
+    return result;
 }
 
 double calcItem(double x, uint16_t n) {
-
+    return (pown(x, n) / fact(n));
 }
 
 double expn(double x, uint16_t count) {
-
+    double result = 0;
+    for (int i = 1; i <= count; i++) {
+        result += calcItem(x, i);
+    }
+    return result;
 }
 
 double sinn(double x, uint16_t count) {
-
+    double result = 0;
+    for (int i = 0; i <= count; i++) {
+        result += calcItem(x, (2 * i) - 1) * pown(-1, i-1);
+    }
+    return result;
 }
 
 double cosn(double x, uint16_t count) {
-
+    double result = 0;
+    for (int i = 1; i <= count; i++) {
+        result += calcItem(x, (2 * i) - 2) * pown(-1, i-1);
+    }
+    return result;
 }
